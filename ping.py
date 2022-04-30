035f9ec68f32f9e82c33b50e3a1f110de4dabc16
drfelliks <104262168+drfelliks@users.noreply.github.com>
2022-04-30 16:20:05 +0200

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,30 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, speed, width, height):
+        super().__init__()
+        self.image = transofrm.scale(image.load(player_image))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 420:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 420:
+            self.rect.y += self.speed
+      
+      
+win_width = 600
+win_height = 500
